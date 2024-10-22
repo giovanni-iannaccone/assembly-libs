1a6234d28cf1edf8bafde64ce3acb734af8f598c
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2024-10-22 17:00:04 +0200

    Add more sockets functions

--- src/sockets.asm
+++ src/sockets.asm
@@ -1,24 +1,49 @@
+;; IMPROVE ME
 accept:
+    mov ebx, 5
+    mov eax, 102
+    int 0x80
     ret
 
-;; Mov a pointer to the list of arguments in ecx before call, 0 to use standard arguments
+;; Mov a pointer to the list of arguments in ecx before call ( ip address, port and addr family )
+;; Mov 0 to ecx to use standard arguments instead
 bind:
-    ret
+    mov edi, eax
+    cmp ecx, 0
+    jnz 
 
+    .default:
+        push dword 0x00000000
+        push word 0x1f40
+        push word 2
+        mov ecx, esp
+        push byte 16
+        push ecx
+        push edi
+        mov ec esp
 
+    .start:
+        mov ebx, 2
+        mov eax, 102
+        int 0x80
+        ret
+
+;; Mov the file descriptor to the stack before call
 close:
     mov ebx, esi
     mov eax, 6
     int 0x80
     ret
 
+;; Mov a pointer to the list of arguments in ecx before call ( max queue lenght and file descriptor )
 listen:
+    mov ebx, 4
+    mov eax, 102
+    int 0x80
     ret
 
-read:
-    ret
-
-;; Mov a pointer to the list of arguments in ecx before call, 0 to use standard arguments
+;; Mov a pointer to the list of arguments in ecx before call ( ip protocol, connection-based protocol and addr family )
+;; Mov 0 to ecx to use standard arguments instead
 socket:
     cmp ecx, 0
     jnz .start
@@ -35,5 +60,5 @@ socket:
         int 0x80
         ret
 
-write:
-    ret
+;; Send and receive calls are not implemented as they are equal to the file write and read. 
+;; Pass the socket file descriptor to the subroutines in files.asm
